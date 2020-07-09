05565365ecea9cdde1061ba855e2b2ba40ecbdaf
James Angeles <JamesAngeles21@gmail.com>
2020-07-09 00:21:38 -0400

    made user have to have a username and made it a primary key

--- green_tea_dating/users/models.py
+++ green_tea_dating/users/models.py
@@ -6,28 +6,17 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 
-class CustomUserManager(BaseUserManager):
-	def create_user(self, email, password, **extra_fields):
-		if not email:
-			raise ValueError("Email must be specified")
-
-		email = self.normalize_email(email)
-		user = self.model(email=email, **extra_fields)
-		user.set_password(password)
-		user.save()
-		return user
-
 class CustomUser(AbstractUser):
-	email = models.EmailField(max_length=254, primary_key=True)
+	username = models.CharField(max_length=30, primary_key=True) 
+	email = models.EmailField(max_length=254, unique=True)
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
 
-	REQUIRED_FIELDS = []
-	USERNAME_FIELD = 'email'
-
-	objects = CustomUserManager()
+	REQUIRED_FIELDS = ["first_name", "last_name"]
 
 	def createUser(user_data):
 		user = CustomUser.objects.create_user(**user_data)
 		return user
 
-	def user_exists(email):
-		return CustomUser.objects.filter(email=email).exists()
+	def user_exists(username):
+		return CustomUser.objects.filter(username=username).exists()
--- green_tea_dating/users/serializers.py
+++ green_tea_dating/users/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = get_user_model()
-		fields = ('email', 'password','first_name', 'last_name')
+		fields = ('username','email', 'password','first_name', 'last_name')
 		extra_kwargs = {
 			'username': {
 				'validators': [UnicodeUsernameValidator()]
@@ -21,8 +21,5 @@ class UserSerializer(serializers.ModelSerializer):
 
 	def update(instance, validated_data):
 		instance.set_password(validated_data['password'])
-		instance.first_name = validated_data['first_name']
-		instance.last_name = validated_data['last_name']
-		instance.email = validated_data['email']
 		instance.save()
 		return instance
